3447d4905d48a9acf5be8b8c47f19e4b625c0b88
Phan Cong Thanh <phancongthanhvtvpvn@gmail.com>
2022-11-26 09:05:36 +0700

    Thêm thông tin customer cho Product

--- backend/src/Domain/Entities/Product.cs
+++ backend/src/Domain/Entities/Product.cs
@@ -1,4 +1,12 @@
 ﻿namespace ProductionMove.Domain.Entities;
+
+public class Customer
+{
+    public string Phone { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+}
+
 public class Product : BaseEntity
 {
     public int Id { get; set; }
@@ -7,9 +15,13 @@ public class Product : BaseEntity
 
     public DateTime DateOfManufacture { get; set; }
 
+    public DateTime SaleDate { get; set; }
+
     public string ProductLineId { get; set; } = string.Empty;
 
     public string FactoryId { get; set; } = string.Empty;
 
     public string DistributionId { get; set; } = string.Empty;
+
+    public virtual Customer Customer { get; set; }
 }
--- backend/src/Infrastructure/Persistence/Configurations/ProductConfiguration.cs
+++ backend/src/Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -12,5 +12,10 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
         builder.HasOne<ProductLine>().WithMany().HasForeignKey(p => p.ProductLineId).OnDelete(DeleteBehavior.Cascade);
         builder.HasOne<Factory>().WithMany(f => f.Products).HasForeignKey(p => p.FactoryId).OnDelete(DeleteBehavior.Cascade);
         builder.HasOne<Distribution>().WithMany().HasForeignKey(p => p.DistributionId).OnDelete(DeleteBehavior.Cascade);
+        builder.OwnsOne(p => p.Customer, customer =>
+        {
+            customer.WithOwner();
+            customer.ToTable(nameof(Customer));
+        });
     }
 }
