c44472104f0a395a50a9c39cf8f3e249856aeee3
NVThang <thang.ludveck@gmail.com>
2022-12-28 18:36:45 +0700

    update loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,26 +13,26 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/login" element={<Login />}/>
-					<Route element={<RequiredAuth allowedRole={RoleSchema.Administrator}/>}>
-						<Route path="/admin/*" element={<Admin />}/>
-						<Route path="*" element={<Missing />}/>
-						<Route path="/unauthorized" element={<Unauthorized />}/>
-					</Route>
-					<Route element={<RequiredAuth allowedRole={RoleSchema.Factory}/>}>
-						<Route path="/factory/*" element={<Factory />}/>
-						<Route path="*" element={<Missing />}/>
-						<Route path="/unauthorized" element={<Unauthorized />}/>
-					</Route>
-					<Route element={<RequiredAuth allowedRole={RoleSchema.Distributor}/>}>
-						<Route path="/distributor/*" element={<Distributor />}/>
-						<Route path="*" element={<Missing />}/>
-						<Route path="/unauthorized" element={<Unauthorized />}/>
-					</Route>  
-					<Route element={<RequiredAuth allowedRole={RoleSchema.ServiceCenter}/>}>
-						<Route path="/serviceCenter/*" element={<ServiceCenter />}/>
-						<Route path="*" element={<Missing />}/>
-						<Route path="/unauthorized" element={<Unauthorized />}/>
-					</Route>
+						<Route element={<RequiredAuth allowedRole={RoleSchema.Administrator}/>}>
+							<Route path="/admin/*" element={<Admin />}/>
+							<Route path="*" element={<Missing />}/>
+							<Route path="/unauthorized" element={<Unauthorized />}/>
+						</Route>
+						<Route element={<RequiredAuth allowedRole={RoleSchema.Factory}/>}>
+							<Route path="/factory/*" element={<Factory />}/>
+							<Route path="*" element={<Missing />}/>
+							<Route path="/unauthorized" element={<Unauthorized />}/>
+						</Route>
+						<Route element={<RequiredAuth allowedRole={RoleSchema.Distributor}/>}>
+							<Route path="/distributor/*" element={<Distributor />}/>
+							<Route path="*" element={<Missing />}/>
+							<Route path="/unauthorized" element={<Unauthorized />}/>
+						</Route>  
+						<Route element={<RequiredAuth allowedRole={RoleSchema.ServiceCenter}/>}>
+							<Route path="/serviceCenter/*" element={<ServiceCenter />}/>
+							<Route path="*" element={<Missing />}/>
+							<Route path="/unauthorized" element={<Unauthorized />}/>
+						</Route>
 				</Routes> 
 			</BrowserRouter>
 		</AuthProvider>
