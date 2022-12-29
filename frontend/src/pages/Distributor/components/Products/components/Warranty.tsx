167445c215e198b84fae35f5d198995a9aa72012
Phan Cong Thanh <phancongthanhvtvpvn@gmail.com>
2022-12-29 19:15:12 +0700

    Update code

--- frontend/src/pages/Distributor/components/Products/components/Warranty.tsx
+++ frontend/src/pages/Distributor/components/Products/components/Warranty.tsx
@@ -4,7 +4,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { Button, Divider, Stack, TextField, Typography } from '@mui/material';
+import { Button, Divider, MenuItem, Stack, TextField, Typography, Select } from '@mui/material';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import DefTextField from '../../../../../components/DefTextField';
@@ -12,6 +12,7 @@ import ConstructionIcon from '@mui/icons-material/Construction';
 import Product from '../../../../../data/entities/Product';
 import backend from '../../../../../backend';
 import useLoading from '../../../../../hooks/useLoading';
+import useBuildings from '../../../../../hooks/useBuildings';
 
 type propTypes = {
     open: boolean,
@@ -28,6 +29,8 @@ const Warranty: FC<propTypes> = (props) => {
     //   handleClose()
     //   resetForm()
     // }
+    const { buildings } = useBuildings();
+    const serviceCenters = buildings?.serviceCenters.map(s => <MenuItem value={s.id}>{s.name}</MenuItem>);
 
     const formik = useFormik({
         initialValues: {
@@ -53,84 +56,46 @@ const Warranty: FC<propTypes> = (props) => {
       });
 
   return (
-    <Dialog open={open} fullWidth>
+  <Dialog open={open} fullWidth>
     <DialogTitle>
-        <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-             <ConstructionIcon/>
-            <Typography variant="h4">Gửi bảo hành</Typography>
-        </Stack>
+      <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+        <ConstructionIcon/>
+        <Typography variant="h4">Gửi bảo hành</Typography>
+      </Stack>
     </DialogTitle>
     <form onSubmit={formik.handleSubmit}>
-    <DialogContent>
-    <Stack
-          direction="row"
-          justifyContent="center"
-          divider={<Divider orientation="vertical" flexItem />}
-        >
-        <Stack spacing={4} justifyContent="center" alignItems = "center" margin={2} flex={1}>
-        <DialogContentText>
-         Thông tin sản phẩm
-      </DialogContentText>
-        <TextField
-                fullWidth
-                label={'Id'}
-                name={'id'}
-                type={'id'}
-                value={row.id}
-                variant="outlined"
-                disabled
-            />
-            <TextField
-                fullWidth
-                label={'Id dòng sản phẩm'}
-                name={'productLineId'}
-                type={'productLineId'}
-                value={row.productLineId}
-                variant="outlined"
-                disabled
-            />
-        </Stack>
-        <Stack spacing={4} justifyContent="center" alignItems = "center" margin={2} flex={1}>
-        <DialogContentText>
-         Thông tin nơi sản xuất
-      </DialogContentText>
-      <TextField
-                fullWidth
-                label={'Id nơi sản xuất'}
-                name={'factoryId'}
-                type={'factoryId'}
-                value={row.factoryId}
-                variant="outlined"
-                disabled
-            />
-            <TextField
-                fullWidth
-                label={'Thời gian sản xuất'}
-                name={'dateOfManufacture'}
-                type={'dateOfManufacture'}
-                value={row.dateOfManufacture.toLocaleString()}
-                variant="outlined"
-                disabled
-            />
-        </Stack>
+      <DialogContent>
+        <Stack direction="row" justifyContent="center" divider={<Divider orientation="vertical" flexItem />} >
+          <Stack spacing={4} justifyContent="center" alignItems = "center" margin={2} flex={1}>
+            <DialogContentText>Thông tin sản phẩm</DialogContentText>
+            <TextField fullWidth label={'Id'} name={'id'} type={'id'} value={row.id} variant="outlined" disabled/>
+            <TextField fullWidth label={'Id dòng sản phẩm'} name={'productLineId'}
+              type={'productLineId'} value={row.productLineId} variant="outlined" disabled />
+          </Stack>
+          <Stack spacing={4} justifyContent="center" alignItems = "center" margin={2} flex={1}>
+            <DialogContentText>Thông tin nơi sản xuất</DialogContentText>
+            <TextField fullWidth label={'Id nơi sản xuất'} name={'factoryId'}
+              type={'factoryId'} value={row.factoryId} variant="outlined" disabled />
+            <TextField fullWidth label={'Thời gian sản xuất'} name={'dateOfManufacture'} type={'dateOfManufacture'}
+              value={row.dateOfManufacture.toLocaleString()} variant="outlined" disabled />
+          </Stack>
         </Stack>
 
         <Stack spacing={4} justifyContent="center" alignItems = "center" margin={2}>
-        <DialogContentText>
-         Nhập nơi bảo hiểm
-      </DialogContentText>
-        <DefTextField
-              formik={formik}
-              label={"id nơi bảo hiểm"}
-              name={"ServiceCenterId"}
-              required
-            />
+          <DialogContentText>Nhập nơi bảo hiểm</DialogContentText>
+          <Select labelId="ServiceCenterId" name='ServiceCenterId' value={formik.values.ServiceCenterId} label="Loại"
+            onChange={formik.handleChange} required>
+              <MenuItem value={"Factory"}>Factory</MenuItem>
+              <MenuItem value={"Distributor"}>Distributor</MenuItem>
+              <MenuItem value={"ServiceCenter"}>ServiceCenter</MenuItem>
+          </Select>
+          {<DefTextField formik={formik} label={"id nơi bảo hiểm"} name={"ServiceCenterId"} required />}
         </Stack>
-    </DialogContent>
-    <DialogActions >
-      <Button onClick={() => {setOpenDialog(false); formik.resetForm()}} variant="outlined">Hủy</Button>
-      <Button variant="contained" type="submit">Xác nhận</Button>
-    </DialogActions>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => {setOpenDialog(false); formik.resetForm()}} variant="outlined">Hủy</Button>
+        <Button variant="contained" type="submit">Xác nhận</Button>
+      </DialogActions>
     </form>
   </Dialog>
   )
