99112148014f1f82aba3a8b941f797ee25a74553
NVThang <thang.ludveck@gmail.com>
2022-12-29 10:31:29 +0700

    fix login

--- frontend/src/components/RequiredAuth.tsx
+++ frontend/src/components/RequiredAuth.tsx
@@ -1,4 +1,4 @@
-import { useLocation, Navigate, Outlet } from "react-router-dom"
+import { useLocation, Navigate, Outlet, useNavigate } from "react-router-dom"
 import useAuth from "../hooks/useAuth"
 import { RoleSchema } from "../data/enums/RoleSchema";
 import { FC } from "react";
@@ -11,12 +11,24 @@ const RequiredAuth: FC<propTypes> = (props)=> {
     const {allowedRole} = props;
     const {auth}  = useAuth()
     const location = useLocation()
+    const navigate = useNavigate()
+
+    const navigateOnRole = (role: RoleSchema) => {
+        if(role === RoleSchema.Administrator) return navigate('/admin/statistics', {replace : true})
+        if(role === RoleSchema.Distributor) return navigate('/distributor/statistics', {replace : true})
+        if(role === RoleSchema.Factory) return navigate('/factory/statistics', {replace : true})
+        if(role === RoleSchema.ServiceCenter) return navigate('/service/statistics', {replace : true})
+        return navigate('/Missing', {replace : true})
+      }
+  
     
     return (
         auth?.user.role === allowedRole
             ? <Outlet />
                 : auth?.user
                 ? <Navigate to="/unauthorized" state={{from: location}} replace/>
+                    // : auth
+                    // ? navigateOnRole(auth?.user.role)
                     : <Navigate to="/login" state={{from: location}} replace/>
     )
 }
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -16,7 +16,7 @@ const Login = () => {
       if(role === RoleSchema.Administrator) return navigate('/admin/statistics', {replace : true})
       if(role === RoleSchema.Distributor) return navigate('/distributor/statistics', {replace : true})
       if(role === RoleSchema.Factory) return navigate('/factory/statistics', {replace : true})
-      if(role === RoleSchema.ServiceCenter) return navigate('/service/statistics', {replace : true})
+      if(role === RoleSchema.ServiceCenter) return navigate('/serviceCenter/statistics', {replace : true})
       return navigate('/Missing', {replace : true})
     }
 
